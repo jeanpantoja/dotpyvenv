302a12573f6f69cd3fbfb668c5bcae690a9ffae2
Jean Carlos Sales Pantoja <je.cspantoja@gmail.com>
2018-01-31 06:58:54 -0200

    adding online installer

---
+++ online_installer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+CURRENT_DIR="$(pwd)"
+CLONE_DIR=/tmp/dotpyvenv
+
+[ -d "${CLONE_DIR}" ] && rm -rfv "${CLONE_DIR}"
+
+git clone https://github.com/jeanpantoja/dotpyvenv.git "${CLONE_DIR}"
+
+cd "${CLONE_DIR}"
+./install.sh
+
+cd "${CURRENT_DIR}"
+
+rm -rfv "${CLONE_DIR}"
