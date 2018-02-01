57f49f3c8b078fe94a9ed861737432396fd138b4
Jean Carlos Sales Pantoja <je.cspantoja@gmail.com>
2018-02-01 09:17:24 -0200

    issue #1 now the virtualenv folder can be set by .pyvenvdir file

--- dotpyvenv.plugin.zsh
+++ dotpyvenv.plugin.zsh
@@ -6,9 +6,17 @@ dotpyvenv:current_dir_has_virtualenv_folder(){
 
 dotpyvenv:source() {
     local DOTPYVENV_DIR=".pyvenv"
+    local DOTPYVENV_FILE=".pyvenvdir"
 
     if [ -n "$VIRTUAL_ENV"  ]; then
         dotpyvenv:current_dir_has_virtualenv_folder || deactivate
+
+    elif [ -f "${DOTPYVENV_FILE}" ]; then
+        DOTPYVENV_DIR="$(cat "${DOTPYVENV_FILE}")"
+
+        [ -f "${DOTPYVENV_DIR}/bin/activate" ] && {
+            source "${DOTPYVENV_DIR}/bin/activate"
+        }
 
     elif [ -f "${DOTPYVENV_DIR}/bin/activate" ]; then
         source "${DOTPYVENV_DIR}/bin/activate"
